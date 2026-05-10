f6db13454355c303a2efdfaf6ccb1cc51595db56
evensliu <m13679035687@163.com>
2026-05-11 04:29:10 +0800

    Fix missing UserService.changePassword interface declaration

--- backend/src/main/java/com/blog/service/UserService.java
+++ backend/src/main/java/com/blog/service/UserService.java
@@ -4,4 +4,6 @@ import com.blog.entity.User;
 
 public interface UserService {
     User getByUsername(String username);
+
+    void changePassword(String username, String oldPassword, String newPassword);
 }
